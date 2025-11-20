703a4944aa32b903ff9d65845873b95a8e2ae3c2
Grego96 <gregoryhunkeler@gmail.com>
2025-11-20 17:56:55 -0300

    struct arbol general

--- src/agGeneros.cpp
+++ src/agGeneros.cpp
@@ -2,50 +2,66 @@
 #include "../include/agGeneros.h"
 #include <assert.h>
 
-
-struct rep_agGeneros {
+struct rep_agGeneros
+{
+  int codigo;
+  char genero[MAX_NOMBRE];
+  TAGGeneros ph;
+  TAGGeneros sh;
 };
 
-TAGGeneros crearTAGGeneros(){
+TAGGeneros crearTAGGeneros()
+{
   return NULL;
 }
 
-void insertarGeneroTAGGeneros(TAGGeneros &arbolGeneros, int idGeneroPadre, int idGenero, const char nombreGenero[MAX_NOMBRE] ){
+void insertarGeneroTAGGeneros(TAGGeneros &arbolGeneros, int idGeneroPadre, int idGenero, const char nombreGenero[MAX_NOMBRE])
+{
 }
 
-void imprimirTAGGeneros(TAGGeneros arbolGeneros){
+void imprimirTAGGeneros(TAGGeneros arbolGeneros)
+{
 }
 
-void liberarTAGGeneros(TAGGeneros &arbolGeneros){
+void liberarTAGGeneros(TAGGeneros &arbolGeneros)
+{
 }
 
-bool existeGeneroTAGGeneros(TAGGeneros arbolGeneros, int idGenero){
+bool existeGeneroTAGGeneros(TAGGeneros arbolGeneros, int idGenero)
+{
   return false;
 }
 
-char* nombreGeneroTAGGeneros(TAGGeneros arbolGeneros, int idGenero){
+char *nombreGeneroTAGGeneros(TAGGeneros arbolGeneros, int idGenero)
+{
   return NULL;
 }
 
-nat alturaTAGGeneros(TAGGeneros arbolGeneros){
+nat alturaTAGGeneros(TAGGeneros arbolGeneros)
+{
   return 0;
 }
 
-nat cantidadTAGGeneros(TAGGeneros arbolGeneros){
+nat cantidadTAGGeneros(TAGGeneros arbolGeneros)
+{
   return 0;
 }
 
-TAGGeneros obtenerSubarbolTAGGeneros(TAGGeneros arbolGeneros, int idGenero) {
+TAGGeneros obtenerSubarbolTAGGeneros(TAGGeneros arbolGeneros, int idGenero)
+{
   return NULL;
 }
 
-TConjuntoGeneros obtenerConjuntoGeneros(TAGGeneros arbolGeneros, int idGenero) {
+TConjuntoGeneros obtenerConjuntoGeneros(TAGGeneros arbolGeneros, int idGenero)
+{
   return NULL;
 }
 
-int obtenerMaxTAGGeneros(TAGGeneros arbolGeneros) {
+int obtenerMaxTAGGeneros(TAGGeneros arbolGeneros)
+{
   return 0;
 }
 
-void removerGeneroTAGGeneros(TAGGeneros &arbolGeneros, int idGenero){
+void removerGeneroTAGGeneros(TAGGeneros &arbolGeneros, int idGenero)
+{
 }
